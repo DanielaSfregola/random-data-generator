db7613cc7adcbfa6631c8a1e8e1ca91ce02dc0c9
Daniela Sfregola <daniela.sfregola@gmail.com>
2017-04-22 11:19:08 +0100

    #39 RandomDataGenerator as object

--- src/main/scala/com/danielasfregola/randomdatagenerator/RandomDataGenerator.scala
+++ src/main/scala/com/danielasfregola/randomdatagenerator/RandomDataGenerator.scala
@@ -1,13 +1,13 @@
 package com.danielasfregola.randomdatagenerator
 
+import com.danielasfregola.randomdatagenerator.utils.{SeedDetector, ShapelessLike}
 import org.scalacheck._
-import org.scalacheck.rng.Seed
 
-import scala.util.Properties
+object RandomDataGenerator extends RandomDataGenerator
 
 trait RandomDataGenerator extends ShapelessLike {
 
-  protected[randomdatagenerator] val seed = RandomDataGenerator.seed
+  protected[randomdatagenerator] val seed = SeedDetector.seed
 
   def random[T](implicit arb: Arbitrary[T]): T = random(1)(arb).head
 
@@ -19,29 +19,3 @@ trait RandomDataGenerator extends ShapelessLike {
 
 }
 
-private[randomdatagenerator] object RandomDataGenerator {
-
-  val SeedVariableName = "RANDOM_DATA_GENERATOR_SEED"
-
-  lazy val seed: Seed = {
-    prettyPrint("info", s"Generating random data with seed $selectedSeed")
-    Seed(selectedSeed)
-  }
-
-  private val selectedSeed = Properties.envOrNone(SeedVariableName).map {
-    seedAsString =>
-      prettyPrint("info", s"Variable $SeedVariableName detected: setting seed to $seedAsString")
-      try {
-        seedAsString.toLong
-      } catch {
-        case ex: Throwable => throw new RuntimeException("Please, provide a numeric seed", ex)
-      }
-  } getOrElse {
-    prettyPrint("info", s"No variable $SeedVariableName detected: setting seed to random number")
-    scala.util.Random.nextLong
-  }
-
-  private def prettyPrint(level: String, msg: String) =
-    println(s"[$level] [RandomDataGenerator] $msg")
-
-}
