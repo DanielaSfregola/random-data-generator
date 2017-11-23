0e936956f48d141bc5ce7dfeb47125accd3ccee8
pkaczor <newion@o2.pl>
2017-11-23 13:14:00 +0100

    fix #48

--- src/main/scala/com/danielasfregola/randomdatagenerator/RandomDataGenerator.scala
+++ src/main/scala/com/danielasfregola/randomdatagenerator/RandomDataGenerator.scala
@@ -2,19 +2,26 @@ package com.danielasfregola.randomdatagenerator
 
 import com.danielasfregola.randomdatagenerator.utils.{SeedDetector, ShapelessLike}
 import org.scalacheck._
+import org.scalacheck.rng.Seed
+
+import scala.annotation.tailrec
 
 object RandomDataGenerator extends RandomDataGenerator
 
 trait RandomDataGenerator extends ShapelessLike {
 
-  protected[randomdatagenerator] val seed = SeedDetector.seed
+  protected[randomdatagenerator] val seed: Seed = SeedDetector.seed
 
   def random[T](implicit arb: Arbitrary[T]): T = random(1)(arb).head
 
-  def random[T](n: Int)(implicit arb: Arbitrary[T]): Seq[T] = {
+  @tailrec
+  final def random[T](n: Int)(implicit arb: Arbitrary[T]): Seq[T] = {
     val gen = Gen.listOfN(n, arb.arbitrary)
     val optSeqT = gen.apply(Gen.Parameters.default, seed)
-    optSeqT.get
+    optSeqT match {
+      case Some(seqT) => seqT
+      case None => random(n)(arb)
+    }
   }
 
 }
