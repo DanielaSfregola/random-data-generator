496655fbd120fed96969c05c7df7bab5c481f4d1
Daniela Sfregola <daniela.sfregola@gmail.com>
2018-02-26 16:16:49 +0000

    #50 Using dedicated exception

---
+++ src/main/scala/com/danielasfregola/randomdatagenerator/RandomDataException.scala
@@ -0,0 +1,3 @@
+package com.danielasfregola.randomdatagenerator
+
+final case class RandomDataException(msg: String) extends Exception(msg)
--- src/main/scala/com/danielasfregola/randomdatagenerator/RandomDataGenerator.scala
+++ src/main/scala/com/danielasfregola/randomdatagenerator/RandomDataGenerator.scala
@@ -11,18 +11,20 @@ trait RandomDataGenerator extends ShapelessLike {
 
   protected[randomdatagenerator] val seed = SeedDetector.seed
 
-  def random[T : WeakTypeTag : Arbitrary]: T = random(1).head
+  def random[T: WeakTypeTag: Arbitrary]: T = random(1).head
 
-  def random[T : WeakTypeTag : Arbitrary](n: Int): Seq[T] = {
+  def random[T: WeakTypeTag: Arbitrary](n: Int): Seq[T] = {
     val gen = Gen.listOfN(n, implicitly[Arbitrary[T]].arbitrary)
     val optSeqT = gen.apply(Gen.Parameters.default, seed)
+    optSeqT.getOrElse(explode)
+  }
+
+  private def explode[T: WeakTypeTag]() = {
     val tpe = implicitly[WeakTypeTag[T]].tpe
-    optSeqT.getOrElse {
-      throw new Exception(s"""
-          Scalacheck could not generate a random value for $tpe.
-          Please, make use that the Arbitrary for type $tpe is not too restrictive
-        """)
-    }
+    val msg =
+      s"""Could not generate a random value for $tpe.
+         |Please, make use that the Arbitrary instance for type $tpe is not too restrictive""".stripMargin
+    throw new RandomDataException(msg)
   }
 
 }
